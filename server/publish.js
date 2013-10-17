8f9b0ec5b8ec772f4c8cf1cdc0db528d74d80200
James Gibson <jamesgibson14@hotmail.com>
2013-10-16 22:40:01 -0600

    using autoform

--- server/publish.js
+++ server/publish.js
@@ -4,3 +4,9 @@
 Meteor.publish("bookings", function () {
   return Bookings.find({});
 });
+Bookings.allow({
+  insert: function (userId, doc) {
+    // the user must be logged in, and the document must be owned by the user
+    return true;
+  }
+})
