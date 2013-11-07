04fb1c7527557c8983c2da002753c210a4b104a9
doug <dgriffin123d@gmail.com>
2013-11-06 19:26:51 -0700

    commiting local changes

--- StgSBooking.js
+++ StgSBooking.js
@@ -1,4 +1,28 @@
 Routes = new Meteor.Collection('Routes');
+Booking = new Meteor.Collection2('booking', {
+  schema: {
+    departureDate: {
+      type: Date,
+      label: "Departure Date"
+    },
+
+    route: {
+      type: String,
+      label: "Departure Route"
+    },
+
+    departureTime: {
+      type: String,
+      label: "Departure Time"
+    },
+
+    price: {
+      type: Number,
+      label: "Total Price"
+    }
+
+  }
+});
 
 if (Meteor.isClient) {
   Template.route_form.insert_route = function () {
