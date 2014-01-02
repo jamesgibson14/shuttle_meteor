d8f11a2d92f6df8c5956774599c0c4015ef1b8bc
James Gibson <jamesgibson14@hotmail.com>
2014-01-02 04:46:36 +0000

    taxi booking inserting into collection

--- client/views/taxiBooking/taxiBooking.js
+++ client/views/taxiBooking/taxiBooking.js
@@ -1,4 +1,5 @@
-Session.setDefault('isReturnRide',true);
+Session.setDefault('isReturnRide', false);
+Session.setDefault('isPickupAtDropoff', null);
 
 Template.taxiBookingForm.rendered = function() {
   if(!this.hasRendered){
@@ -6,12 +7,17 @@ Template.taxiBookingForm.rendered = function() {
     $('.datepicker').datepicker({
       onSelect: function(){
         var pickup = $('#pickupDate').val();
-        console.log(depart);
+        console.log(pickup);
         Session.set('selectedPickupDate', pickup);
       },
       minDate: new Date()
     });
     $('#pickupTime').timepicker({
+      minuteStep: 5,
+      showInputs: false,
+      disableFocus: true
+    });
+    $('#returnPickupTime').timepicker({
       minuteStep: 5,
       showInputs: false,
       disableFocus: true
@@ -23,35 +29,85 @@ Template.taxiBookingForm.events({
   'change .selectReturnRide': function(e, temp){
     if(e.target.value === "round-trip"){
       Session.set('isReturnRide',true);
-      console.log(Session.get('isReturnRide'));
+      console.log("isReturnRide = " + Session.get('isReturnRide'));
     }
     else {
       Session.set('isReturnRide', false);
-      console.log(Session.get('isReturnRide'));
+      Session.set('isPickupAtDropoff', null);
+      console.log("isReturnRide = " + Session.get('isReturnRide')) + " and " + Session.get('isPickupAtDropoff');
     }
   },
-  /*
-  'click #booking-submit': function(e, temp){
-    var booking = {}
-    booking.returnTrip = temp.find('.selectReturnRide:checked').value;
-    booking.departDate = temp.find('#departDate').value;
-    booking.returnDate = temp.find('#returnDate').value;
-    booking.pickUp = temp.find('#pickUp').value;
-    booking.dropOff = temp.find('#dropOff').value;
-    booking.departTime = temp.find('#departureTime').value;
-    booking.departSeat = temp.find('.departSeat:checked').value;
-    booking.departPassengers = temp.find('#departurePassengers').value;
-    booking.returnTime = temp.find('#returnTime').value;
-    booking.returnSeat = temp.find('.returnSeat:checked').value;
-    booking.returnPassengers = temp.find('#returnPassengers').value;
-    booking.returnFare = (booking.pickUp == 'Las Vegas' || booking.dropOff == 'Las Vegas') ? 39 : 59; //pickUpAdjustment + dropOffAdjustment + roundTripAdjustment + returnTimeAdjustment
-    booking.departFare = (booking.pickUp == 'Las Vegas' || booking.dropOff == 'Las Vegas') ? 39 : 59; //pickUpAdjustment + dropOffAdjustment + roundTripAdjustment + departTimeAdjustment
-    booking.totalFare = booking.departFare + booking.returnFare;
-    booking.totalPassengers = parseInt(booking.returnPassengers) + parseInt(booking.departPassengers);
-    console.log(booking);
-    Bookings.insert(booking);
+  
+  'change .selectDifferentPickup': function(e, temp){
+    if ((e.target.value === "pickupAtDropoff") && Session.get('isReturnRide')){
+      Session.set('isPickupAtDropoff',true);
+      console.log("isPickupAtDropoff " + Session.get('isPickupAtDropoff'));
+    }
+    else if ((e.target.value === "differentPickup") && Session.get('isReturnRide')){
+      Session.set('isPickupAtDropoff', false);
+      console.log("isPickupAtDropoff " + Session.get('isPickupAtDropoff'));
+    }
+    else {
+      Session.set('isPickupAtDropoff', null);
+    }
+  },
+  
+  'click #taxiBooking-submit': function(e, temp){
+    var booking = {};
+    booking.type = "taxi";
+    booking.name = temp.find('#fullName').value;
+    booking.phone = temp.find('#phoneNumber').value;
+    booking.email = temp.find('#emailAddress').value;
+    booking.pickupLocation = temp.find('#pickupLocation').value + ", " + temp.find('#pickupAddress').value;
+    booking.passengerCount = temp.find('#passengerCount').value;
+    booking.pickupDate = temp.find('#pickupDate').value;
+    booking.pickupTime = temp.find('#pickupTime').value;
+    var dateTimeStr =  booking.pickupDate + " " + booking.pickupTime;
+    booking.pickupAt = moment(dateTimeStr, "MM/DD/YYYY h:mm a").toDate();
+    booking.destinationLocation = temp.find('#destinationLocation').value + ", " + temp.find('#destinationAddress').value;
+    booking.returnRide = temp.find('.selectReturnRide:checked').value;
     
-  }*/
+    if (Session.get('isReturnRide')) {
+      var returnBooking = _.clone(booking);
+      if(Session.get('isPickupAtDropoff')) {
+        returnBooking.pickupLocation = temp.find('#destinationLocation').value + ", " + temp.find('#destinationAddress').value;
+        returnBooking.pickupDate = temp.find('#returnPickupDate').value;
+        returnBooking.pickupTime = temp.find('#returnPickupTime').value;
+        var datTimeStr = returnBooking.pickupDate + " " + returnBooking.pickupTime;
+        returnBooking.pickupAt = moment(dateTimeStr, "MM/DD/YYYY h:mm a").toDate();
+        returnBooking.destinationLocation = temp.find('#pickupLocation').value + ", " + temp.find('#pickupAddress').value;
+      }
+      
+      if(temp.find('.selectDifferentPickup:checked').value === "differentPickup") {
+        returnBooking.pickuptLocation = temp.find('#returnPickupLocation').value + ", " + temp.find('#returnPickupAddress').value;
+        returnBooking.pickupDate = temp.find('#returnPickupDate').value;
+        returnBooking.pickupTime = temp.find('#returnPickupTime').value;
+        var datTimeStr = returnBooking.pickupDate + " " + returnBooking.pickupTime;
+        returnBooking.pickupAt = moment(dateTimeStr, "MM/DD/YYYY h:mm a").toDate();
+        returnBooking.destinationLocation = temp.find('#pickupLocation').value + ", " + temp.find('#pickupAddress').value;
+      }
+    }
+    
+   
+    console.log(booking, returnBooking);
+    var departId = "";
+    var onReturnBookingInsert = function(err, id){
+      if(!err){
+        Bookings.update({_id: departId},{$set: {nextBookingId: id}})
+      }
+    }
+    Bookings.insert(booking, function(err, id){
+      if(!err){
+        if(returnBooking){
+          departId = id;
+          Bookings.insert(returnBooking, onReturnBookingInsert); 
+        }
+      }
+      else
+        console.log(err);
+    });   
+    
+  }
   
 });
 
@@ -59,7 +115,35 @@ Template.taxiBookingForm.helpers({
   isReturnRide: function(){
     return Session.get('isReturnRide');
   },
+  isPickupAtDropoff: function(){
+    if (Session.get('isReturnRide') && (Session.get('isPickupAtDropoff') != null)) {
+      return Session.get('isPickupAtDropoff')
+    }
+    else return false;
+  },
+  isDifferentPickup: function(){
+    if (Session.get('isReturnRide') && (Session.get('isPickupAtDropoff') != null)) {
+      return !Session.get('isPickupAtDropoff')
+    }
+    else return false;
+  },
   today: function(){
      return moment().format('MM/DD/YYYY');
   }
 });
+Template.drivers.helpers({
+  drivers: function(){
+    return [{name: "James"},{name: "Shane"},{name: "Doug"},{name: "Alice"},{name: "Bob"}]
+  }
+})
+
+Template.taxiRuns.helpers({
+  runs: function() {
+    return Bookings.find({type: "taxi"}, {sort: {pickupAt: -1}});
+  }
+})
+Template.taxiCart.helpers({
+  cartTotal: function(){
+    return 13.15;
+  }
+})
