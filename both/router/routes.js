96235957a181240faf6b4463e09940cbf2ec0472
James Gibson <jamesgibson14@hotmail.com>
2014-04-11 22:05:19 +0000

    clockin

---
+++ both/router/routes.js
@@ -0,0 +1,82 @@
+Router.configure({
+  layoutTemplate: 'master_layout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  yieldTemplates: {
+    header: {
+      to: 'header'
+    },
+    footer: {
+      to: 'footer'
+    },
+    sidebar: {
+      to: 'sidebar'
+    }
+  }
+});
+
+authController = RouteController.extend({
+  onBeforeAction: function(pause){
+    var currentUser = Meteor.user();
+    if(!currentUser) {
+      this.render('entrySignIn');
+      pause();
+    }
+  }
+})
+
+Router.map(function(){
+  this.route('taxiHome', { path: '/'});
+  this.route('reports', {
+    path: '/Reports',
+    controller: authController
+  });
+  this.route('admin', {
+    path: '/admin',
+    controller: authController
+  });
+  this.route('userAccount', {
+    path: '/UserAccount',
+    controller: authController
+  });
+  this.route('shuttleBooking', {
+    path: '/ShuttleBooking',
+    controller: authController
+  });
+  this.route('runHistory', {
+    path: '/RunHistory',
+    controller: authController
+  });
+  this.route('taxiBookingForm', {
+    path: '/TaxiBooking',
+    controller: authController
+  });
+  this.route('driverView', {
+    path: '/DriverView',
+    controller: authController
+  });
+  this.route('email', {
+    path: '/email',
+    controller: authController
+  });
+  this.route('clockinApp', {
+    path: '/clockin',
+    controller: authController,
+    yieldTemplates: {
+      'clockin': {
+        to: 'aside'
+      },
+      header: {
+        to: 'header'
+      },
+      footer: {
+        to: 'footer'
+      },
+      sidebar: {
+        to: 'sidebar'
+      }
+    }
+  });
+  this.route('dashboard', {path: '/Dashboard'});
+  this.route('notFound', {path: '*'});
+})
---
+++ client/views/timeclock/clockin.js
@@ -0,0 +1,45 @@
+Meteor.setInterval(function () {
+  Session.set('time', new Date);
+}, 1000);
+Template.clockin.events({
+  'click .clock-in': function(){
+    var doc = {};
+    doc.time = Session.get('time');
+    doc.user = prompt('Enter your pin?');
+
+    Records.insert(doc);
+  }
+})
+Template.clockin.helpers({
+  hours: _.range(0, 12),
+  degrees: function () {
+    return 30 * this;
+  },
+  handData: function () {
+    var time = Session.get('time') || new Date;
+    return { hourDegrees: time.getHours() * 30,
+      minuteDegrees: time.getMinutes() * 6,
+      secondDegrees: time.getSeconds() * 6 };
+  },
+  radial: function (angleDegrees,
+                    startFraction,
+                    endFraction) {
+    var r = 100;
+    var radians = (angleDegrees-90) / 180 * Math.PI;
+
+    return {
+      x1: r * startFraction * Math.cos(radians),
+      y1: r * startFraction * Math.sin(radians),
+      x2: r * endFraction * Math.cos(radians),
+      y2: r * endFraction * Math.sin(radians)
+    };
+  },
+  getDate: function(){
+    return  Session.get('time') || new Date;
+  }
+});
+Template.clockinApp.helpers({
+  getHistory: function(){
+    return Records.find({}, {sort: {time: -1}});
+  }
+})
